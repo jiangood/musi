a35b23df6f2d6cf99c6af2cd5806da7638d8ebae
jiangood <jiangood@users.noreply.github.com>
2026-05-21 16:20:06 +0000

    chore: bump version to 2.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.android.build.gradle.internal.api.ApkVariantOutputImpl
 import java.util.Properties
 
-val appVersionName = "2.4.0"
+val appVersionName = "2.4.2"
 val localProperties = Properties()
 val localPropertiesFile: File = rootProject.file("local.properties")
 if (localPropertiesFile.exists()) {
@@ -52,7 +52,7 @@ android {
         applicationId = "fumi.day.literalmusi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
+        versionCode = 13
         versionName = appVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
